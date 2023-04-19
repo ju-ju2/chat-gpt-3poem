8b92fc9efefa3ee7a1950441648da6abddf975f0
한주연 <hjy2859@naver.com>
2023-04-19 16:47:27 +0900

    화면 css 구성

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+// module.exports = {
+//   env: {
+//     NEXT_PUBLIC_CHAT_API_KEY: process.env.NEXT_PUBLIC_CHAT_API_KEY,
+//   },
+// };
