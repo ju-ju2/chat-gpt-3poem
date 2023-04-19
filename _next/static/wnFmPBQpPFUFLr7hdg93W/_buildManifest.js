22bfef59b26ca4df7178d0f4d9c892697040860e
한주연 <hjy2859@naver.com>
2023-04-19 22:33:09 +0900

    deploy to gh-pages

---
+++ _next/static/wnFmPBQpPFUFLr7hdg93W/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/740-94ea40387467ede6.js","static/chunks/pages/index-74dcc2cf2c6b7340.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
